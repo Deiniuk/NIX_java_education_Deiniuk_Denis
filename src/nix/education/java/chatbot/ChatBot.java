1433f99e6f203a3b58efe752239d87ded801dccf
Deiniuk <d.s.deiniuk@student.khai.edu>
2021-10-11 13:59:17 +0300

    ChatBot 1-st stage

---
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -0,0 +1,11 @@
+package nix.education.java.chatbot;
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        String name = "Amogus";
+        int year = 2020;
+        System.out.println("Hello! My name is " + name + ".\n" +
+                "I was created in " + year + ".");
+    }
+}
